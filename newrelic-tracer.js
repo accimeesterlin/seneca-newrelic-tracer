a4485990dcf7886213d24a2956fdb25e23fe4b9f
Montgomery, Richard (CAI - Atlanta) <Richard.Montgomery@coxautoinc.com>
2018-01-29 12:12:02 -0500

    add custom params to new relic

--- newrelic-tracer.js
+++ newrelic-tracer.js
@@ -1,16 +1,41 @@
 const Newrelic = require('newrelic');
 
-function newrelicInward(ctx, data) {
-    const servicePin = data.msg.meta$.pattern;
+let customParamsHandler;
 
+function isInternalAction(msg) {
+    return msg.role === 'seneca' ||
+      msg.role === 'transport' ||
+      msg.role === 'options' ||
+      msg.role === 'mesh' ||
+      msg.init;
+}
+
+function newrelicInward(ctx, { msg, err, res }) {
+    if (isInternalAction(msg)) {
+        return;
+    }
+
+    const servicePin = msg.meta$.pattern;
     Newrelic.setTransactionName(servicePin);
+}
+function newrelicOutward(ctx, { msg, err, res }) {
+    if (isInternalAction(msg)) {
+        return;
+    }
+
+    if (customParamsHandler) {
+        const params = customParamsHandler(msg, err, res);
+        Newrelic.addCustomParameters(params);
+    }
 }
 
 
-function newrelicTracer() {
+function newrelicTracer(opts) {
     const seneca = this;
-
+    const { customParameterHandler } = opts;
+    customParamsHandler = customParameterHandler;
     seneca.inward(newrelicInward);
+    seneca.outward(newrelicOutward);
 }
 
 module.exports = newrelicTracer;
