089b261af87af8401aa25228dcbdf82d98b39a5d
Montgomery, Richard (CAI - Atlanta) <Richard.Montgomery@coxautoinc.com>
2018-02-06 20:04:20 -0500

    revert tracer

--- newrelic-tracer.js
+++ newrelic-tracer.js
@@ -1,41 +1,16 @@
 const Newrelic = require('newrelic');
 
-let customParamsHandler;
+function newrelicInward(ctx, data) {
+    const servicePin = data.msg.meta$.pattern;
 
-function isInternalAction(msg) {
-    return msg.role === 'seneca' ||
-      msg.role === 'transport' ||
-      msg.role === 'options' ||
-      msg.role === 'mesh' ||
-      msg.init;
-}
-
-function newrelicInward(ctx, { msg, err, res }) {
-    if (isInternalAction(msg)) {
-        return;
-    }
-
-    const servicePin = msg.meta$.pattern;
     Newrelic.setTransactionName(servicePin);
 }
-function newrelicOutward(ctx, { msg, err, res }) {
-    if (isInternalAction(msg)) {
-        return;
-    }
-
-    if (customParamsHandler) {
-        const params = customParamsHandler(msg, err, res);
-        Newrelic.addCustomParameters(params);
-    }
-}
 
 
-function newrelicTracer(opts) {
+function newrelicTracer() {
     const seneca = this;
-    const { customParameterHandler } = opts;
-    customParamsHandler = customParameterHandler;
+
     seneca.inward(newrelicInward);
-    seneca.outward(newrelicOutward);
 }
 
 module.exports = newrelicTracer;
